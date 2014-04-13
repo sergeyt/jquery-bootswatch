6848a02f1632e9653a06e428826f33c5828f37d5
sergeyt <stodyshev@gmail.com>
2014-04-13 22:33:34 +0700

    revised theme loading (#3)

--- jquery.bootswatch.js
+++ jquery.bootswatch.js
@@ -8,19 +8,8 @@
 		var themes = 'default,amelia,cerulean,cosmo,cyborg,flatly,journal,lumen,readable,simplex,slate,spacelab,superhero,united,yeti'.split(',');
 
 		// templates
-		var link_template = '<link data-theme="$name" rel="stylesheet" href="http://bootswatch.com/$name/bootstrap.css" disabled="disabled"/>';
+		var link_template = '<link data-theme="$name" type="text/css" rel="stylesheet" href="http://bootswatch.com/$name/bootstrap.css" />';
 		var item_template = '<li><a href="#" data-theme="$name"><span>$label</span>&nbsp;<span class="glyphicon"></span></a></li>';
-
-		// insert theme links
-		if (!$('link[data-theme]').length) {
-			themes
-				.filter(function(name) { return name != 'default'; })
-				.forEach(function(name) {
-					var html = link_template.replace(/\$name/g, name);
-					$(html).appendTo($('head'));
-				});
-		}
-
 		var selected_icon = 'glyphicon-ok';
 
 		// insert theme menu items
@@ -38,9 +27,14 @@
 		$this.find('a').click(function() {
 			var theme = $(this).data('theme');
 
-			// toggle theme link
-			$('link[data-theme]').attr('disabled', 'disabled');
-			$('link[data-theme=' + theme + ']').removeAttr('disabled');
+			// inject theme
+			var link = $('link[data-theme]');
+			if (link.length) {
+				link.attr('href', "http://bootswatch.com/$name/bootstrap.css".replace(/\$name/g, theme))
+					.attr("data-theme", theme);
+			} else {
+				$(link_template.replace(/\$name/g, theme)).appendTo($('head'));
+			}
 
 			// toggle icon
 			$('a[data-theme] .' + selected_icon).removeClass(selected_icon);
